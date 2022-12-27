d262495622754db7ddb7c4b2741b7730b67b5bdb
Sludge <alicembrooks@protonmail.com>
2022-12-27 12:02:24 +0000

    Change revoke old rsa key and add ed25519 key

--- users/root.nix
+++ users/root.nix
@@ -1,7 +1,7 @@
 {...}: {
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYG9DW+A7mec59SdhQgBD5DwJJgBuL1lQPf3kaHRztPMxZB957Uim5Z2WrBLDJnAdsYTr5xa0tiy4luwHukYx0At57KsGGHjcqsmEUi6anmtGnJ6XJtU1F4qdhgxgx890qdoufByu8H/n+oYHfMBFHQW38ldDRTzB9dxbTq5qqryO4SqQVAl0eEVder2JTTWRT+0DzAzkvXj3V01H5lNFIqTqlcnSk9ZJ20j92dA/H4pvYM89psx4iOjgwg9tqFvkduIFY8FjT7Zn1+qgnergvFC3ik6kh4mLmyoLX0+WpxTPJaGiZ48f8z0iRdvNXKq7gr7F1t170ojebBgagojinhjsMpdJTnoDqRm589q1UY56xoXDZoZYTzZ6HIMaz0uBQqDXjXFLoMDsiK9FgrmE0LIhEWbqXaHlIH4YebL+OPpjLOVLuAmKAN8Q71JoWlZlskhSxh2e6nlutz/sW0+pqMl6R4uljYD1ebGUdQAsqPRkcXDfcgODLjMI2xepCi2E= sludge@pop-os"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEdwOxk6/VaBO+X/DCggz5r0IR8Zp/3k2aQEtt3Oq9L7 sludge@wonderland"
     ];
   };
 }
