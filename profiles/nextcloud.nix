7313b243a076c071a7c52aeb07cdecfa52197a2e
Sludge <alicembrooks@protonmail.com>
2023-06-27 22:01:24 +0100

    Bump nextcloud version

--- profiles/nextcloud.nix
+++ profiles/nextcloud.nix
@@ -5,7 +5,7 @@
 }: {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud26;
+    package = pkgs.nextcloud27;
     enableBrokenCiphersForSSE = false;
     autoUpdateApps.enable = true;
     hostName = "nextcloud.sludge.network";
