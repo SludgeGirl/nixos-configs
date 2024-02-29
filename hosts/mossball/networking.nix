a4668e20fe5795e3c78dc14a67f0bb3b3d9dcd66
Sludge <alicembrooks@protonmail.com>
2024-02-29 06:43:46 +0000

    Correct nix styling according to alejandra

--- hosts/mossball/networking.nix
+++ hosts/mossball/networking.nix
@@ -1,37 +1,47 @@
 {
-    networking = {
-        useDHCP = true;
-        usePredictableInterfaceNames = false;
-        interfaces.enp0s31f6 = {
-        useDHCP = false;
-        ipv4.addresses = [
-            {
-            # WARNING: MAIN IP DO NOT TOUCH
-            address = "94.130.48.76";
-            prefixLength = 26;
-            }
-        ];
-        ipv6.addresses = [
-            {
-            # WARNING: MAIN IP DO NOT TOUCH
-            address = "2a01:4f8:10b:25e0::1";
-            prefixLength = 64;
-            }
-            {
-            address = "fe80::921b:eff:febd:9b39";
-            prefixLength = 64;
-            }
-        ];
-        ipv4.routes = [ { address = "94.130.48.64"; prefixLength = 26; } ];
-        ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
-        };
-
-        defaultGateway6 = {
-        address = "fe80::1";
-        interface = "enp0s31f6";
-        };
-
-        defaultGateway = "94.130.48.65";
-        nameservers = [ "8.8.8.8" "8.8.4.4" ];
+  networking = {
+    useDHCP = true;
+    usePredictableInterfaceNames = false;
+    interfaces.enp0s31f6 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          # WARNING: MAIN IP DO NOT TOUCH
+          address = "94.130.48.76";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          # WARNING: MAIN IP DO NOT TOUCH
+          address = "2a01:4f8:10b:25e0::1";
+          prefixLength = 64;
+        }
+        {
+          address = "fe80::921b:eff:febd:9b39";
+          prefixLength = 64;
+        }
+      ];
+      ipv4.routes = [
+        {
+          address = "94.130.48.64";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.routes = [
+        {
+          address = "fe80::1";
+          prefixLength = 128;
+        }
+      ];
     };
+
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp0s31f6";
+    };
+
+    defaultGateway = "94.130.48.65";
+    nameservers = ["8.8.8.8" "8.8.4.4"];
+  };
 }
--- profiles/git-talk.nix
+++ profiles/git-talk.nix
@@ -8,20 +8,19 @@
     forceSSL = true;
 
     locations."/" = {
-        proxyPass = "http://localhost:4000";
+      proxyPass = "http://localhost:4000";
 
-        extraConfig =
-            "proxy_ssl_server_name on;"
-        ;
+      extraConfig = "proxy_ssl_server_name on;";
     };
-  };  virtualisation.oci-containers.backend = "podman";
+  };
+  virtualisation.oci-containers.backend = "podman";
 
   virtualisation.oci-containers.containers = {
     git-talk = {
       image = "alectair/git-talk";
       autoStart = true;
-      ports = [ "4000:4000" ];
-      extraOptions = [ "--pull=always" ];
+      ports = ["4000:4000"];
+      extraOptions = ["--pull=always"];
     };
   };
 }
