6f89b672e51876a241c4b2962c11fbb67d126b46
Sludge <alicembrooks@protonmail.com>
2024-03-22 12:10:09 +0000

    Clean up mumble config

--- profiles/murmur.nix
+++ profiles/murmur.nix
@@ -8,21 +8,8 @@
     openFirewall = true;
     password = "$MURMURD_PASSWORD";
     environmentFile = "/var/murmur.env";
-    # sslCert = "/var/lib/acme/mumble.sludge.network/fullchain.pem";
-    # sslKey = "/var/lib/acme/mumble.sludge.network/key.pem";
     registerName = "Hidie Room";
     registerHostname = "murmur.sludge.network";
     logFile = "/var/log/murmur/murmurd.log";
   };
-
-  # config.security.acme = {
-  #   certs."mumble.sludge.network" = {
-  #     group = "mumble-sludge-network";
-  #     dnsProvider = "cloudflare";
-  #     dnsPropagationCheck = true;
-  #     credentialsFile = "/var/cloudflare.env";
-  #     reloadServices = ["murmur"];
-  #   };
-  # };
-  # config.users.groups."mumble-sludge-network".members = ["murmur"];
 }
