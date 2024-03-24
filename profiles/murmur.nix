1f28cc4a2bfef63025ba1358b2dfb78bd4e30eb5
Sludge <alicembrooks@protonmail.com>
2024-03-24 15:18:15 +0000

    Add ssl for murmur

--- profiles/murmur.nix
+++ profiles/murmur.nix
@@ -8,21 +8,17 @@
     openFirewall = true;
     password = "$MURMURD_PASSWORD";
     environmentFile = "/var/murmur.env";
-    # sslCert = "/var/lib/acme/mumble.sludge.network/fullchain.pem";
-    # sslKey = "/var/lib/acme/mumble.sludge.network/key.pem";
+    sslCert = "/var/lib/acme/mumble.sludge.network/fullchain.pem";
+    sslKey = "/var/lib/acme/mumble.sludge.network/key.pem";
     registerName = "Hidie Room";
     registerHostname = "murmur.sludge.network";
     logFile = "/var/log/murmur/murmurd.log";
   };
 
-  # config.security.acme = {
-  #   certs."mumble.sludge.network" = {
-  #     group = "mumble-sludge-network";
-  #     dnsProvider = "cloudflare";
-  #     dnsPropagationCheck = true;
-  #     credentialsFile = "/var/cloudflare.env";
-  #     reloadServices = ["murmur"];
-  #   };
-  # };
   # config.users.groups."mumble-sludge-network".members = ["murmur"];
+  config.security.acme.certs."mumble.sludge.network" = {
+    dnsProvider = "rfc2136";
+    credentialsFile = "/var/glauca.env";
+    reloadServices = ["murmur"];
+  };
 }
