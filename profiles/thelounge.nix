97d022950b98b5f57e62f3a5a95f161e5bd77145
Sludge <alicembrooks@protonmail.com>
2024-05-11 13:59:40 +0100

    Add thelounge cleanup task

--- profiles/thelounge.nix
+++ profiles/thelounge.nix
@@ -28,5 +28,26 @@
     };
   };
 
+  systemd.services.thelounge-file-cleanup = {
+    description = "Cleanup thelounge uploads";
+    requires = ["multi-user.target"];
+    after = ["multi-user.target"];
+
+    serviceConfig = {
+      Type = "oneshot";
+      SuccessExitStatus = "0";
+      ExecStart = "find /var/lib/thelounge/uploads/ -type f -mtime +7 -print -delete";
+    };
+  };
+
+  systemd.timers.thelounge-file-cleanup = {
+    description = "Time for cleaning up thelounge file uploads";
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "*-*-* 09:00:00";
+      Unit = "thelounge-file-cleanup.service";
+    };
+  };
+
   networking.firewall.allowedTCPPorts = [80 443];
 }
