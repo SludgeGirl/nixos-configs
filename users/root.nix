0b8d9f8eafce944beb3eaaaa13d945284708caac
Sludge <alicembrooks@protonmail.com>
2025-03-27 21:05:58 +0000

    Update ssh keys

--- users/root.nix
+++ users/root.nix
@@ -1,10 +1,9 @@
 {...}: {
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEdwOxk6/VaBO+X/DCggz5r0IR8Zp/3k2aQEtt3Oq9L7 sludge@wonderland"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjYQ9IGw3aWonTyNv2mAMd24fmBC3DjgQPbLPD9yTRY sludge@maidcafe"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKGc/5Ob0s5PQfs6bHYozP3/xMqyLVtW3imo5iRCAA9"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXakbTs0Cy7F+OLMtfGT1jCKK+xkM1phdFuYq4PfUNw sludge@vermillion"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbw+AVDQKQn8qiYNFgzN1CNHNnv4fnmZmbeXbk7a013 sludge@apothecium"
     ];
   };
 }
